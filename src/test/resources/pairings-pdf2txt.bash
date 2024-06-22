20b3a4f7a7a8399151b25753fcec9bb13ddf0d2f
Stephan Heinemann <stephan@rigiresearch.com>
2024-06-22 10:32:32 -0700

    #4: added project details, license plugin and upgraded versions

--- src/test/resources/pairings-pdf2txt.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for i in ./pdfpairings/*.pdf; do
-    [ -f "$i" ] || break;
-    echo "converting \"$i\" to \"${i%.*}.txt\" ...";
-    pdf2txt -W 0.5 "$i" | col -bx | tr -s ' '> "${i%.*}.txt";
-done
-
-mv pdfpairings/*.txt txtpairings/
